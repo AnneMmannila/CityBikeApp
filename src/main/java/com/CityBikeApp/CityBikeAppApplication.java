7515ea74443a124fd6889c7d2bf93db263d40e0d
AnneMmannila <annemarimannila@gmail.com>
2022-08-10 20:02:07 +0300

    initialize spring project

---
+++ src/main/java/com/CityBikeApp/CityBikeAppApplication.java
@@ -0,0 +1,13 @@
+package com.CityBikeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBikeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityBikeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CityBikeApp/CityBikeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CityBikeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBikeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
