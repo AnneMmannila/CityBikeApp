6c02efc58f118efd877e46134a1b735c626b91f5
AnneMmannila <annemarimannila@gmail.com>
2022-08-11 21:48:22 +0300

    Add allow CORS for frontend

--- src/main/java/com/CityBikeApp/controller/CityBikeAppController.java
+++ src/main/java/com/CityBikeApp/controller/CityBikeAppController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,6 +18,7 @@ public class CityBikeAppController {
 	@Autowired
 	private JourneyRepository repository;
 
+	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping(value = "/")
 	public List<Journey> allJourneys(Model model) {
 		List<Journey> journeys = repository.findAll();
