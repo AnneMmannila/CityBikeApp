310ce0cdff5ae3ecaeb75870c4a155736189ca90
AnneMmannila <annemarimannila@gmail.com>
2022-08-13 19:02:06 +0300

    Add rest for single station, clean up code

--- src/main/java/com/CityBikeApp/controller/CityBikeAppController.java
+++ src/main/java/com/CityBikeApp/controller/CityBikeAppController.java
@@ -1,11 +1,12 @@
 package com.CityBikeApp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,22 +20,28 @@ import com.CityBikeApp.station.StationRepository;
 public class CityBikeAppController {
 	@Autowired
 	private JourneyRepository repository;
-	
+
 	@Autowired
 	private StationRepository stationRepository;
 
 	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping(value = "/")
-	public List<Journey> allJourneys(Model model) {
+	public List<Journey> allJourneys() {
 		List<Journey> journeys = repository.findAll();
 		return journeys;
 	}
 
 	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping(value = "/stationslist")
-	public List<Station> allStations(Model model) {
+	public List<Station> allStations() {
 		List<Station> stations = stationRepository.findAll();
-
 		return stations;
+	}
+
+	@CrossOrigin(origins = "http://localhost:8080")
+	@RequestMapping(value = "/find/{id}")
+	public Optional<Station> singleStation(@PathVariable("id") Long stationId) {
+		Optional<Station> station = stationRepository.findById(stationId);
+		return station;
 	}
 }
